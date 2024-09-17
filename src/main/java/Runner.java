2ee5c3b176b74dda9bcd325d97f7e3bd4610ad7e
nesibe <tnesibe.techproed@gmail.com>
2024-09-17 21:15:22 +0300

    tech3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -7,6 +7,7 @@ public class Runner {
         System.out.println("versiyon 3");
         System.out.println("techpro 1");
         System.out.println("techpro 2");
+        System.out.println("techpro 3");
 
     }
 }
